fb0f74013c9ab72b90d2cc9dfd2be54f70723a12
dporr <dporras.mail@gmail.com>
2020-05-12 00:21:55 -0600

    import abort library

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -1,5 +1,5 @@
 import json
-from flask import request, _request_ctx_stack
+from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
@@ -31,7 +31,7 @@ class AuthError(Exception):
     return the token part of the header
 '''
 def get_token_auth_header():
-   auth = request.headers.get('Authorization', None)
+    auth = request.headers.get('Authorization', None)
     if not auth:
         raise AuthError({
         'code': 'authorization_header_missing',
